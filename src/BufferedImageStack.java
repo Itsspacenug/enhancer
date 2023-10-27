5411d920f5e979b8c0c54b226966888b95d8b06b
Itsspacenug <aaln22006@gmail.com>
2023-10-27 14:37:37 -0500

    Update BufferedImageStack.java

--- src/BufferedImageStack.java
+++ src/BufferedImageStack.java
@@ -1,7 +1,52 @@
+import java.awt.image.BufferedImage;
 
 public class BufferedImageStack {
 
 	public static void main(String[] args) {
 		
+	}
+	//enters buffered image into stack and returns nothing 
+	//If this would exceed the capacity of the array,
+	//then a new array should be allocated having double the size of the old array,
+	//and the old array's elements copied to the new array.
+	private void push(BufferedImage someBufferedImage) {
+		
+	}
+	
+	/*throws an exception if the stack is empty; otherwise returns the top
+	buffered image, removing it from the stack. The exception should be an
+	instance of java.util.EmptyStackException. In this assignment, you are not
+	required to ever replace a large array by a smaller array when the number of
+	stack elements decreases because of pop operations.*/
+	public BufferedImage pop() {
+		return null;
+		
+	}
+	
+	//returns true if there are no items in the stack; false otherwise.
+	public boolean isEmpty() {
+		return false;
+	}
+	
+	/*returns the buffered image at the position given by the index.
+	This is not commonly available in a stack, but it facilitates the testing by our
+	autograder.) If the index is out of range, the method should throw an
+	IndexOutOfBounds exception. (Note: get(0) gets the bottom element of the
+	stack -- the one that was pushed in first, but not yet popped out.)*/
+	public BufferedImage get(int index) {
+		return null;
+	}
+	
+	//returns the number elements currently in the stack.
+	public int getSize() {
+		return 0;
+	}
+	
+	/*returns the current size of the array being used to hold the
+	stack elements. Like the get operation, this is not a normal stack operation,
+	but may be used by the grading software to assure compliance with the
+	specifications.*/
+	public int getArraySize() {
+		return 0;
 	}
 }
